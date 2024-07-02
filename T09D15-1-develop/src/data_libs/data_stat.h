bf8de6b614944db60ff5a14e72075bf5ad32037d
Salnikov-Nikita <salniknik@gmail.com>
2024-07-02 10:33:32 +0300

    Delete T09D15-1-develop directory

--- T09D15-1-develop/src/data_libs/data_stat.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATA_STAT_H
-#define DATA_STAT_H
-
-double max(double *data, int n);
-double min(double *data, int n);
-double mean(double *data, int n);
-double variance(double *data, int n);
-
-#endif
--- T09D15-1-develop/src/data_module/data_process.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROCESSING_H
-#define PROCESSING_H
-
-#define EPS 1E-6
-
-int normalization(double *data, int n);
-
-#endif
