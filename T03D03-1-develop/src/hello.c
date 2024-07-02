427f927ba424a17bca001586757cd2819d16f962
Salnikov-Nikita <salniknik@gmail.com>
2024-07-02 10:32:55 +0300

    Delete T03D03-1-develop directory

--- T03D03-1-develop/src/hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, AI!");
-    return 0;
-}
--- T03D03-1-develop/src/named_hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int a = 0;
-    scanf("%d", &a);
-    printf("Hello, %d!", a);
-    return 0;
-}
